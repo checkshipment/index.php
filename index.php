78052d2501d4123e7eab5aecf64eea3700ae0358
checkshipment <136893439+checkshipment@users.noreply.github.com>
2023-06-19 11:34:26 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Black Page Example</title>
+  <style>
+    body {
+      background-color: black;
+      color: white;
+    }
+
+    .button {
+      display: inline-block;
+      background-color: #337ab7;
+      color: white;
+      padding: 10px 20px;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+  </style>
+</head>
+<body>
+  <p><a href="https://checkyourshipment.com/trax-tracking/">Trax Courier</a> has been founded by Muhammad Hasan Khan in 2017. It is an end-to-end logistics and supply chain solutions provider catering to the e-commerce industry. The company is committed to delivering the most innovative logistics services to ensure its customers receive the best possible service. Trax offers warehouses across Pakistan to provide quick deliveries and reliable inventory management solutions. Additionally, the company offers the fastest transfer of funds and a responsive customer experience to support the growth of online businesses.</p>
+
+  <a href="https://checkyourshipment.com/" class="button">Check Your Shipment</a>
+</body>
+</html>
